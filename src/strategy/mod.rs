f099f1a6c76d403da7fc01348457b8796ef275de
neil <neil@neils-MacBook-Pro.local>
2022-09-10 11:47:06 +0800

    finish strategy pattern

--- src/strategy/mod.rs
+++ src/strategy/mod.rs
@@ -3,51 +3,76 @@
 1 - program to an interface over implementation, Run Time vs Compile Time, Program to Supertype
 */
 
-struct Duck {
-    name: String,
-    description: String // display
-    // soundBehaviour: SoundBehaviour - enum
+trait SoundBehaviour {
+    fn make_sound(&self, duck: &Duck);
 }
 
-// enum SoundBehaviour {
+struct Duck {
+    name: String,
+    // description: String, // display
+    sound_behaviour: Box<dyn SoundBehaviour>,
+}
 
-// }
+struct DefaultQuack;
+impl SoundBehaviour for DefaultQuack {
+    fn make_sound(&self, duck: &Duck) {
+        println!("{}: quack", duck.name);
+    }
+}
+
+struct SqueakQuack;
+impl SoundBehaviour for SqueakQuack {
+    fn make_sound(&self, duck: &Duck) {
+        println!("{}: squeak", duck.name);
+    }
+}
 
 impl Default for Duck {
     fn default() -> Self {
         Duck {
-            name: "unknown".to_string(),
-            description: "a generic duck".to_string()
+            name: "nameless".to_string(),
+            // description: "a generic duck".to_string(),
+            sound_behaviour: Box::new(DefaultQuack{})
         }
     }
 }
 
 trait ConstantDuckTraits {
-    fn perform_sound(&self);
-    fn perform_fly(&self);
-    fn swim(&self);
+    fn quack(&self);
+    fn set_quack_behaviour(&mut self, sound_behaviour: Box<dyn SoundBehaviour>);
 }
 
 impl ConstantDuckTraits for Duck {
-    fn perform_sound(&self) {
-        println!("try make sound! {}", self.name);
+    fn quack(&self) {
+        self.sound_behaviour.make_sound(self);
     }
-    fn swim(&self) {
-        println!("swimming! {}", self.description);
-    }
-    fn perform_fly(&self) {
-        println!("try flying! {}", self.name)
+    fn set_quack_behaviour(&mut self, sound_behaviour: Box<dyn SoundBehaviour>) {
+        self.sound_behaviour = sound_behaviour;
     }
 }
 
 
 
 pub fn run() {
-    let duck1 = Duck {
+    // let "mut" is important because we are mutating it in fn set_quack_behaviour
+    // in a way its an additional cogload to users(developers) but intellisense catches it easily
+    let mut default_duck = Duck {
         name: "donald".to_string(),
         ..Default::default()
     };
-    duck1.swim();
-    duck1.perform_fly();
-    duck1.perform_sound();
+    default_duck.quack();
+
+    let rubber_duck: Duck = Duck {
+        name: "Rubber Duck".to_string(),
+        sound_behaviour: Box::new(SqueakQuack{})
+    };
+
+    rubber_duck.quack();
+
+    default_duck.set_quack_behaviour(Box::new(SqueakQuack{}));
+    default_duck.quack();
+
+    // Not going to work unless let "mut" rubber_duck = 
+    // rubber_duck.set_quack_behaviour(Box::new(DefaultQuack{}));
+    // rubber_duck.quack();
 }
