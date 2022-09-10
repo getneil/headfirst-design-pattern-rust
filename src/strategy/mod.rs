e079aab88324f587454def09e63e3ebf2429417b
neil <neil@neils-MacBook-Pro.local>
2022-09-10 11:51:34 +0800

    additional notes

--- src/strategy/mod.rs
+++ src/strategy/mod.rs
@@ -53,9 +53,16 @@ impl ConstantDuckTraits for Duck {
 
 
 
+/**
+ * Notes:
+ *  - there is no such thing as class extend in Rust, I initially wanted something like struct RubberDuck extends Duck
+ *  - mastering Ownership concept is important
+ */
 pub fn run() {
     // let "mut" is important because we are mutating it in fn set_quack_behaviour
     // in a way its an additional cogload to users(developers) but intellisense catches it easily
+    // this would help -> https://doc.rust-lang.org/book/ch04-00-understanding-ownership.html
+    // Ownership is a unique concept in Rust would help a lot if you understand it
     let mut default_duck = Duck {
         name: "donald".to_string(),
         ..Default::default()
@@ -75,4 +82,6 @@ pub fn run() {
     // Not going to work unless let "mut" rubber_duck = 
     // rubber_duck.set_quack_behaviour(Box::new(DefaultQuack{}));
     // rubber_duck.quack();
+
+
 }
